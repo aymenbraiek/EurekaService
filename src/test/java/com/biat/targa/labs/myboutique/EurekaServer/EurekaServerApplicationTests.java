4a5b2200a969f543997b50e6900db1bbb7bbcfc2
aymenbraiek <aymen.braiek@esprit.tn>
2021-05-12 17:39:03 +0100

    adding Spotify Maven Plugin +Dockerfile

---
+++ src/test/java/com/biat/targa/labs/myboutique/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biat.targa.labs.myboutique.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
